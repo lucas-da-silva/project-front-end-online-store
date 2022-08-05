fccdb50fe19c32c2761fd766d3701aa8f897e94b
lucas-da-silva <lucassouza00012345@gmail.com>
2022-08-05 15:29:45 -0300

    req-1 done

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
-export async function getCategories() {
-  // Implemente aqui
+async function fetchData(url) {
+  const object = await fetch(url);
+  const json = object.json();
+  return json;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getCategories() {
+  const url = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const data = await fetchData(url);
+  return data;
+}
+
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const url = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  const data = await fetchData(url);
+  return data;
 }

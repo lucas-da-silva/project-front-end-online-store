378cd224cbde816406e9bbe27be16fd151221854
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-09 10:09:57 -0300

    Lógica principal feita

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import CartCounter from '../components/CartCounter';
 
 class Cart extends Component {
   render() {
     return (
       <section>
-        <p data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </p>
+        <div data-testid="shopping-cart-empty-message">
+          <div>
+            Celular
+            <label htmlFor="price">
+              R$
+              <input id="price" type="number" value={ 500 } readOnly />
+            </label>
+          </div>
+          <CartCounter itemPrice={ 500 } />
+        </div>
       </section>
     );
   }

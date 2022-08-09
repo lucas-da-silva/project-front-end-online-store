face6d37f0978e19f192b3fda23c41a29055e3e4
Tiago Laureano <tiagodiaslaureano32@gmail.com>
2022-08-09 15:40:09 -0300

    req-8 done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
+import { getProductsStorage } from '../services/storage';
 
 class Cart extends Component {
+  state = {
+    products: [],
+  }
+
+  componentDidMount() {
+    const products = getProductsStorage();
+    this.setState({ products });
+  }
+
   render() {
+    const { products } = this.state;
     return (
       <section>
-        <p data-testid="shopping-cart-empty-message">
-          Seu carrinho está vazio
-        </p>
+        {
+          products.length <= 0 ? (
+            <p data-testid="shopping-cart-empty-message">
+              Seu carrinho está vazio
+            </p>
+          ) : (
+            products.map(({ id, title, quantity }) => (
+              <div key={ id }>
+                <span data-testid="shopping-cart-product-name">
+                  {title}
+                </span>
+                <br />
+                <span data-testid="shopping-cart-product-quantity">
+                  {quantity}
+                </span>
+              </div>
+            ))
+          )
+        }
       </section>
     );
   }

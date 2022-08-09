d7564637036149df58911d8d3b491306233b246d
Gabriel <gabrielproencaaraujo@gmail.com>
2022-08-09 15:23:46 -0300

    req-07 done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import CartCounter from '../components/CartCounter';
 
 class Cart extends Component {
   render() {
     return (
       <section>
         <div data-testid="shopping-cart-empty-message">
-          <div>
-            Celular
-            <label htmlFor="price">
-              R$
-              <input id="price" type="number" value={ 500 } readOnly />
-            </label>
-          </div>
-          <CartCounter itemPrice={ 500 } />
+          Seu carrinho está vazio
         </div>
       </section>
     );
